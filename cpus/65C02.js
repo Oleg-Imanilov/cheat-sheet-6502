5e794163b59dc925d05af90419f63d229483a1d1
oleg.im@gmail.com <oleg.im@gmail.com>
2019-10-13 14:19:44 -0500

    added 65c02

---
+++ cpus/65C02.js
@@ -0,0 +1,577 @@
+
+const cpu65C02 = [
+    {
+        category: 'Load & Store',
+        name: 'LDA',
+        flags: '<N>VBDI<Z>C',
+        title: '<L>oa<D> <A>ccumulator',
+    },
+    {
+        category: 'Load & Store',
+        name: 'LDX',
+        flags: '<N>VBDI<Z>C',
+        title: '<L>oa<D> <X> register',
+    },
+
+    {
+        category: 'Load & Store',
+        name: 'LDY',
+        flags: '<N>VBDI<Z>C',
+        title: '<L>oa<D> <Y> register',
+    },
+
+    {
+        category: 'Load & Store',
+        name: 'STA',
+        flags: 'NVBDIZC',
+        title: '<ST>ore <A>ccumulator',
+    },
+
+    {
+        category: 'Load & Store',
+        name: 'STX',
+        flags: 'NVBDIZC',
+        title: '<ST>ore <X> register',
+    },
+    {
+        category: 'Load & Store',
+        name: 'STY',
+        flags: 'NVBDIZC',
+        title: '<ST>ore <Y> register',
+    },
+    {
+        category: 'Load & Store',
+        name: 'STZ',
+        flags: 'NVBDIZC',
+        title: '<ST>ore <Z>ero',
+    },
+    {
+        category: 'Stack',
+        name: 'PHA',
+        flags: 'NVBDIZC',
+        title: '<P>us<H> <A>ccumulator',
+    },
+
+    {
+        category: 'Stack',
+        name: 'PHX',
+        flags: 'NVBDIZC',
+        title: '<P>us<H> <X> register',
+    },
+
+    {
+        category: 'Stack',
+        name: 'PHY',
+        flags: 'NVBDIZC',
+        title: '<P>us<H> <Y> register',
+    },
+
+    {
+        category: 'Stack',
+        name: 'PHP',
+        flags: 'NVBDIZC',
+        title: '<P>us<H> <P>rocessor status',
+
+    },
+
+    {
+        category: 'Stack',
+        name: 'PLA',
+        flags: 'NVBDIZC',
+        title: '<P>u<L>l <A>ccumulator',
+
+    },
+    {
+        category: 'Stack',
+        name: 'PLP',
+        flags: '<NVBDIZC>',
+        title: '<P>u<L>l <P>rocessor status',
+
+    },
+    {
+        category: 'Stack',
+        name: 'PLX',
+        flags: '<N>VBDI<Z>C',
+        title: '<P>u<L>l <X> register',
+
+    },
+
+    {
+        category: 'Stack',
+        name: 'PLY',
+        flags: '<N>VBDI<Z>C',
+        title: '<P>u<L>l <Y> register',
+
+    },
+
+    {
+        category: 'Stack',
+        name: 'TXS',
+        flags: '<N>VBDI<Z>C',
+        title: '<T>ransfer <X> to <S>tack',
+
+    },
+    {
+        category: 'Stack',
+        name: 'TSX',
+        flags: 'NVBDIZC',
+        title: '<T>ransfer <S>tack to <X>',
+
+    },
+
+    {
+        category: 'Calc',
+        name: 'INA',
+        flags: '<N>VBDI<Z>C',
+        title: '<IN>crement <A>ccumulator',
+
+    },
+    {
+        category: 'Calc',
+        name: 'INX',
+        flags: '<N>VBDI<Z>C',
+        title: '<IN>crement <X>',
+
+    },
+    {
+        category: 'Calc',
+        name: 'INY',
+        flags: '<N>VBDI<Z>C',
+        title: '<IN>crement <Y>',
+
+    },
+    {
+        category: 'Calc',
+        name: 'DEA',
+        flags: '<N>VBDI<Z>C',
+        title: '<DE>crement <A>ccumulator',
+
+    },
+    {
+        category: 'Calc',
+        name: 'DEX',
+        flags: '<N>VBDI<Z>C',
+        title: '<DE>crement <X>',
+
+    },
+    {
+        category: 'Calc',
+        name: 'DEY',
+        flags: '<N>VBDI<Z>C',
+        title: '<DE>crement <Y>',
+
+    },
+    {
+        category: 'Calc',
+        name: 'INC',
+        flags: '<N>VBDI<Z>C',
+        title: '<INC>rement memory',
+
+    },
+    {
+        category: 'Calc',
+        name: 'DEC',
+        flags: '<N>VBDI<Z>C',
+        title: '<DEC>rement memory',
+
+    },
+    {
+        category: 'Calc',
+        name: 'ADC',
+        title: '<AD>D with <C>arry',
+        mode: { Im: 2, Z: 3, Zx: 4, Ab: 4 },
+
+        flags: '<NV>BDI<ZC>'
+    },
+    {
+        category: 'Calc',
+        name: 'SBC',
+        flags: '<NV>BDI<ZC>',
+        title: '<S>u<B>tract with <C>arry',
+
+    },
+    {
+        category: 'Bits',
+        name: 'ASL',
+        flags: '<N>VBDI<ZC>',
+        title: '<A>rithmetic <S>hift <L>eft',
+
+    },
+    {
+        category: 'Bits',
+        name: 'LSR',
+        flags: '<N>VBDI<ZC>',
+        title: '<L>ogical <S>hift <R>ight',
+
+    },
+    {
+        category: 'Bits',
+        name: 'ROL',
+        flags: '<N>VBDI<ZC>',
+        title: '<RO>tate <L>eft',
+
+    },
+    {
+        category: 'Bits',
+        name: 'ROR',
+        flags: '<N>VBDI<ZC>',
+        title: '<RO>tate <R>ight',
+
+    },
+    {
+        category: 'Bits',
+        name: 'AND',
+        flags: '<N>VBDI<Z>C',
+        title: 'bitwise <AND> with accumulator',
+
+    },
+    {
+        category: 'Bits',
+        name: 'ORA',
+        flags: '<N>VBDI<Z>C',
+        title: 'bitwize <OR> with <A>ccumulator',
+
+    },
+    {
+        category: 'Bits',
+        name: 'EOR',
+        flags: '<N>VBDI<Z>C',
+        title: 'bitwise <E>xclusive <OR>',
+
+    },
+    {
+        category: 'Bits',
+        name: 'BIT',
+        flags: '<NV>BDI<Z>C',
+        title: 'test <BIT>s',
+
+    },
+    {
+        category: 'Bits',
+        name: 'CMP',
+        flags: '<N>VBDI<ZC>',
+        title: '<C>o<MP>are accumulator',
+
+    },
+    {
+        category: 'Bits',
+        name: 'CPX',
+        flags: '<N>VBDI<ZC>',
+        title: '<C>om<P>are <X> register',
+
+    },
+    {
+        category: 'Bits',
+        name: 'CPY',
+        flags: '<N>VBDI<ZC>',
+        title: '<C>om<P>are <Y> register',
+
+    },
+    {
+        category: 'Bits',
+        name: 'TRB',
+        flags: 'NVBDIZC<X>',
+        title: '<T>est and <R>eset <B>its',
+
+    },
+    {
+        category: 'Bits',
+        name: 'TSB',
+        flags: 'NVBDIZC<X>',
+        title: '<T>est and <S>et <B>its',
+
+    },
+
+    {
+        category: 'Bits',
+        name: 'RMB',
+        flags: 'NVBDIZC<X>',
+        title: '<R>eset <M>emory <B>it',
+
+    },
+
+    {
+        category: 'Bits',
+        name: 'SMB',
+        flags: 'NVBDIZC<X>',
+        title: '<S>et <M>emory <B>it',
+
+    },
+    {
+        category: 'Flow',
+        name: 'JMP',
+        flags: 'NVBDIZC',
+        title: '<J>u<MP>',
+
+    },
+    {
+        category: 'Flow',
+        name: 'JSR',
+        flags: 'NVBDIZC',
+        title: '<J>ump to <S>ub<R>outine',
+
+    },
+    {
+        category: 'Flow',
+        name: 'RTS',
+        flags: 'NVBDIZC',
+        title: '<R>e<T>urn from <S>ubroutine',
+
+    },
+    {
+        category: 'Flow',
+        name: 'RTI',
+        flags: '<NVBDIZC>',
+        title: '<R>e<T>urn from <I>nterrupt',
+
+    },
+    {
+        category: 'Flow',
+        name: 'BRA',
+        flags: 'NVBDIZC',
+        title: '<BR>anch <A>lways',
+
+    },
+    {
+        category: 'Flow',
+        name: 'BEQ',
+        flags: 'NVBDIZC',
+        title: '<B>ranch on <EQ>ual',
+
+    },
+    {
+        category: 'Flow',
+        name: 'BNE',
+        flags: 'NVBDIZC',
+        title: '<B>ranch on <N>ot <E>qual',
+
+    },
+    {
+        category: 'Flow',
+        name: 'BCC',
+        flags: 'NVBDIZC',
+        title: '<B>ranch on <C>arry <C>lear',
+
+    },
+    {
+        category: 'Flow',
+        name: 'BCS',
+        flags: 'NVBDIZC',
+        title: '<B>ranch on <C>arry <S>et',
+
+    },
+    {
+        category: 'Flow',
+        name: 'BVC',
+        flags: 'NVBDIZC',
+        title: '<B>ranch on o<V>erflow <C>lear',
+
+    },
+    {
+        category: 'Flow',
+        name: 'BVS',
+        flags: 'NVBDIZC',
+        title: '<B>ranch on o<V>erflow <S>et',
+
+    },
+    {
+        category: 'Flow',
+        name: 'BMI',
+        flags: 'NVBDIZC',
+        title: '<B>ranch on <MI>nus',
+
+    },
+    {
+        category: 'Flow',
+        name: 'BPL',
+        flags: 'NVBDIZC',
+        title: '<B>ranch on <PL>us',
+
+    },
+    {
+        category: 'Flow',
+        name: 'BBR<0-7>',
+        flags: 'NVBDIZC',
+        title: '<B>ranch on <B>it <R>eset',
+
+    },
+    {
+        category: 'Flow',
+        name: 'BBS<0-7>',
+        flags: 'NVBDIZC',
+        title: '<B>ranch on <B>it <S>et',
+
+    },
+
+    {
+        category: 'State',
+        name: 'CLC',
+        flags: 'NVBDIZC',
+        title: '<CL>ear <C>arry',
+
+    },
+    {
+        category: 'State',
+        name: 'CLD',
+        flags: 'NVBDIZC',
+        title: '<CL>ear <D>ecimal',
+
+    },
+    {
+        category: 'State',
+        name: 'CLI',
+        flags: 'NVBDIZC',
+        title: '<CL>ear <I>nterrupt',
+
+    },
+    {
+        category: 'State',
+        name: 'CLV',
+        flags: 'NVBDIZC',
+        title: '<CL>ear o<V>erflow',
+
+    },
+    {
+        category: 'State',
+        name: 'SEC',
+        flags: 'NVBDIZC',
+        title: '<SE>t <C>arry',
+
+    },
+
+    {
+        category: 'State',
+        name: 'SED',
+        flags: 'NVBDIZC',
+        title: '<SE>t <D>ecimal',
+
+    },
+    {
+        category: 'State',
+        name: 'SEI',
+        flags: 'NVBDIZC',
+        title: '<SE>t <I>nterrupt',
+
+    },
+    {
+        category: 'Transfer',
+        name: 'TAX',
+        flags: '<N>VBDI<Z>C',
+        title: '<T>ransfer <A> to <X>',
+
+    },
+
+    {
+        category: 'Transfer',
+        name: 'TAY',
+        flags: '<N>VBDI<Z>C',
+        title: '<T>ransfer <A> to <Y>',
+
+    },
+    {
+        category: 'Transfer',
+        name: 'TXA',
+        flags: '<N>VBDI<Z>C',
+        title: '<T>ransfer <X> to <A>',
+
+    },
+    {
+        category: 'Transfer',
+        name: 'TYA',
+        flags: '<N>VBDI<Z>C',
+        title: '<T>ransfer <Y> to <A>',
+
+    },
+
+    {
+        category: 'Misc',
+        name: 'BRK',
+        flags: 'NV<B>DIZC',
+        title: '<BR>ea<K>',
+
+    },
+
+    {
+        category: 'Misc',
+        name: 'NOP',
+        flags: 'NVBDIZC',
+        title: '<N>o <OP>eration',
+
+    },
+
+]
+
+export const timing = {
+    ADC: { Im: 2, Z: 3, Zx: 4, Ab: 4, Ax: 4, Ay: 4, Ix: 6, Iy: 5, Iz: 5, },
+    AND: { Im: 2, Z: 3, Zx: 4, Ab: 4, Ax: 4, Ay: 4, Ix: 6, Iy: 5, Iz: 5, },
+    ASL: { Ac: 2, Z: 5, Zx: 6, Ab: 6, Ax: 7, },
+    'BBR<0-7>': { R: 2, },
+    'BBS<0-7>': { R: 2, },
+    BCC: { R: 2, },
+    BCS: { R: 2, },
+    BEQ: { R: 2, },
+    BIT: { Im: 2, Z: 3, Zx: 4, Ab: 4, Ax: 4, },
+    BMI: { R: 2, },
+    BNE: { R: 2, },
+    BPL: { R: 2, },
+    BRA: { R: 3, },
+    BRK: { Ip: 7, },
+    BVC: { R: 2, },
+    BVS: { R: 2, },
+    CLC: { Ip: 2, },
+    CLD: { Ip: 2, },
+    CLI: { Ip: 2, },
+    CLV: { Ip: 2, },
+    CMP: { Im: 2, Z: 3, Zx: 4, Ab: 4, Ax: 4, Ay: 4, Ix: 6, Iy: 5, Iz: 5, },
+    CPX: { Im: 2, Z: 3, Ab: 4, },
+    CPY: { Im: 2, Z: 3, Ab: 4, },
+    DEA: { Ac: 2, },
+    DEC: { Z: 5, Zx: 6, Ab: 6, Ax: 7, },
+    DEX: { Ip: 2, },
+    DEY: { Ip: 2, },
+    EOR: { Im: 2, Z: 3, Zx: 4, Ab: 4, Ax: 4, Ay: 4, Ix: 6, Iy: 5, Iz: 5, },
+    INA: { Ac: 2, },
+    INC: { Z: 5, Zx: 6, Ab: 6, Ax: 7, },
+    INX: { Ip: 2, },
+    INY: { Ip: 2, },
+    JMP: { Ab: 3, Ia: 5, Iax: 6, },
+    JSR: { Ab: 6, },
+    LDA: { Im: 2, Z: 3, Zx: 4, Ab: 4, Ax: 4, Ay: 4, Ix: 6, Iy: 5, Iz: 5, },
+    LDX: { Im: 2, Z: 3, Zy: 4, Ab: 4, Ay: 4, },
+    LDY: { Im: 2, Z: 3, Zy: 4, Ab: 4, Ay: 4, },
+    LSR: { Ac: 2, Z: 5, Zx: 6, Ab: 6, Ax: 7, },
+    NOP: { Ip: 2, },
+    ORA: { Im: 2, Z: 3, Zx: 4, Ab: 4, Ax: 4, Ay: 4, Ix: 6, Iy: 5, Iz: 5, },
+    PHA: { Ip: 3, },
+    PHX: { Ip: 3, },
+    PHY: { Ip: 3, },
+    PHP: { Ip: 3 },
+    PLP: { Ip: 4 },
+    PLA: { Ip: 4, },
+    PLX: { Ip: 4, },
+    PLY: { Ip: 4, },
+    ROL: { Ac: 2, Z: 5, Zx: 6, Ab: 6, Ax: 7, },
+    ROR: { Ac: 2, Z: 5, Zx: 6, Ab: 6, Ax: 7, },
+    RTI: { Ip: 6, },
+    RTS: { Ip: 6, },
+    SBC: { Im: 2, Z: 3, Zx: 4, Ab: 4, Ax: 4, Ay: 4, Ix: 6, Iy: 5, Iz: 5, },
+    SEC: { Ip: 2, },
+    SED: { Ip: 2, },
+    SEI: { Ip: 2, },
+    STA: { Z: 3, Zx: 4, Ab: 4, Ax: 5, Ay: 5, Ix: 6, Iy: 6, Iz: 5, },
+    STX: { Z: 3, Zy: 4, Ab: 4, },
+    STY: { Z: 3, Zx: 4, Ab: 4, },
+    STZ: { Z: 3, Zx: 4, Ab: 4, Ax: 5, },
+    TAX: { Ip: 2, },
+    TAY: { Ip: 2, },
+    TRB: { Z: 5, Ab: 6, },
+    TSB: { Z: 5, Ab: 6, },
+    RMB: { Z: 5, Ab: 6, },
+    SMB: { Z: 5, Ab: 6, },
+    TSX: { Ip: 2, },
+    TXA: { Ip: 2, },
+    TXS: { Ip: 2, },
+    TYA: { Ip: 2, },
+}
+
+export default cpu65C02;
---
+++ pages/65C02.js
@@ -0,0 +1,180 @@
+import Ttl from '../components/Ttl';
+
+import commands, {timing} from '../cpus/65C02';
+
+function Cmd({name, title, flags, modes = '', bg='#FFF' }) {
+    const mode = timing[name] || {'?????':0}
+    const mm = Object.keys(mode).map((k, i)=>{ return <span key={i} style={{display:'inline-block',color:'#777',paddingRight:'6px'}}>{k}<sup style={{color:'#060'}}>{mode[k]}</sup></span> })
+    return <div className='card flex-item' style={{background:bg}}>
+        <div className='cmdName'>{name}</div>
+        <span style={{ float: 'right' }}>
+            <Ttl txt={flags} />
+        </span><br />
+        <Ttl size={12} color='black' txt={title} /><br />
+        <div className="activeModes">
+            {mm}
+        </div>
+    </div>
+}
+
+function Modes() {
+    return (
+        <div className='card flex-item'>
+            <table style={{ margin: '4px' }}>
+                <tbody>
+                    <tr><td><Ttl size={12} color='#080' txt='<I>m<p>lied' /></td><td></td></tr>                    
+                    <tr><td><Ttl size={12} color='#080' txt='<Ac>cumulator' /></td><td></td></tr>
+                    <tr><td><Ttl size={12} color='#080' txt='<Im>mediate' /></td><td>#$12</td></tr>
+                    <tr><td><Ttl size={12} color='#080' txt='<Z>eroPage' /></td><td>$12</td></tr>
+                    <tr><td><Ttl size={12} color='#080' txt='<Z>eroPage,<x>' /></td><td>$12,X</td></tr>
+                    <tr><td><Ttl size={12} color='#080' txt='<Z>eroPage,<y>' /></td><td>$12,Y</td></tr>
+                    <tr><td><Ttl size={12} color='#080' txt='<Ab>solute' /></td><td>$1234</td></tr>
+                    <tr><td><Ttl size={12} color='#080' txt='<A>bsolute,<x>' /></td><td>$1234,X</td></tr>
+                    <tr><td><Ttl size={12} color='#080' txt='<A>bsolute,<y>' /></td><td>$1234,Y</td></tr>
+                    <tr><td><Ttl size={12} color='#080' txt='<R>elative' /></td><td>LABEL</td></tr>
+                    <tr><td><Ttl size={12} color='#080' txt='<I>nd, zp <x>' /></td><td>($12,X)</td></tr>
+                    <tr><td><Ttl size={12} color='#080' txt='<I>nd, <a>bs <x>' /></td><td>($1234,X)</td></tr>                    
+                    <tr><td><Ttl size={12} color='#080' txt='<I>nd, zp <y>' /></td><td>($12),Y</td></tr>
+                    <tr><td><Ttl size={12} color='#080' txt='<I>nd, <z>p' /></td><td>($12)</td></tr>                    
+                    <tr><td><Ttl size={12} color='#080' txt='<I>nd, <a>bs' /></td><td>($1234)</td></tr>                    
+                </tbody>
+            </table>
+            <style jsx>{`
+                td {
+                    font-size: 10px;
+                }
+                td:last-child {
+                    text-align:right;
+                    color: #F0F;
+                }
+            `}
+            </style>
+        </div>
+    );
+}
+
+export const categories = [
+    'Load & Store',
+    'Stack',
+    'Bits',
+    'Flow',
+    'State',
+    'Transfer',
+    'Misc',
+    'Calc',
+];
+const catCol = [
+    '#efe',
+    '#eff',
+    '#eef',
+    '#fef',    
+    '#ffe',
+    '#fee',    
+    '#fed',
+    '#def'    
+]
+
+
+function Cat({ix}) {
+    const category = categories[ix];
+    const bg=catCol[ix] || '#eee';
+    return (<div className="category" style={{backgroundColor:bg}}>
+        <div style={{textAlign:'center'}}>{ix} {category}</div>
+        {commands.filter(d => d.category == category).map(d => <Cmd {...d} />)}    
+    </div>)
+}
+
+
+function CatList({ix}) {
+    return <>
+        <div className='flex-item cat-title' style={{background:catCol[ix]}}>{categories[ix]}</div>
+        {commands.filter(d => d.category === categories[ix]).map(d => <Cmd bg={catCol[ix]} {...d} />)}
+    </>
+}
+
+export default function () {
+    return (<div>
+        <div style={{textAlign:'center',fontSize:'18px',color:'gray'}}>65C02 INSTRUCTIONS</div>
+        <div className='flex-container' style={{height:'1220px'}}>
+            <div className='flex-item cat-title'>Modes</div>
+            <Modes />
+            <CatList ix={0}/>
+            <CatList ix={4}/>
+            <CatList ix={2}/>
+            <CatList ix={3}/>
+            <CatList ix={1}/>
+            <CatList ix={5}/>                        
+            <CatList ix={7}/>                        
+            <CatList ix={6}/>
+        </div>
+        <style jsx global>{`
+            @font-face {
+                font-family: c64pro;
+                src:url("/static/C64_Pro-STYLE.ttf");
+            }
+            * {
+                font-family: c64pro;
+                font-size: 14px;
+            }
+            .card {
+                width: 200px;
+                display: inline-block;
+                padding:1px;
+                border:2px solid silver;
+                margin-top:4px;
+            }
+            .cmdName {
+                display: inline-block;
+                color: white;
+                background: black;
+                padding: 3px;
+            }
+            .activeModes {
+                text-align:right;
+                margin-top: 2px;
+                padding-top:2px;
+                border-top: solid 1px silver;
+            }
+            .cat-title {
+                text-align: center;
+                padding: 3px;
+                border: 1px solid silver;
+                border-radius: 4px;
+                margin-top: 8px;
+            }
+            .flex-container {
+                display: -ms-flexbox;
+                display: -webkit-flex;
+                display: flex;
+                -webkit-flex-direction: column;
+                -ms-flex-direction: column;
+                flex-direction: column;
+                -webkit-flex-wrap: wrap;
+                -ms-flex-wrap: wrap;
+                flex-wrap: wrap;
+                -webkit-justify-content: flex-start;
+                -ms-flex-pack: start;
+                justify-content: space-between;
+                -webkit-align-content: center;
+                -ms-flex-line-pack: center;
+                align-content: center;
+                -webkit-align-items: stretch;
+                -ms-flex-align: stretch;
+                align-items: stretch;
+            }
+            .flex-item {
+                margin-right: 4px;
+                -webkit-order: 0;
+                -ms-flex-order: 0;
+                order: 0;
+                -webkit-flex: 0 1 auto;
+                -ms-flex: 0 1 auto;
+                flex: 0 1 auto;
+                -webkit-align-self: auto;
+                -ms-flex-item-align: auto;
+                align-self: auto;
+            }
+        `}</style>
+
+    </div>)
+}
